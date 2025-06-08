c4009258f709da2d549fd81be38906cb5fff2c6a
ChanminK <thisischanmin@gmail.com>
2025-06-08 17:32:41 -0400

    Made the inital files

---
+++ main.js
@@ -0,0 +1,8 @@
+const gameBoard = document.getElementById('game-board');
+
+for (let i = 0; i < 16; i++) {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    tile.textContent = '2';
+    gameBoard.appendChild(title);
+}
